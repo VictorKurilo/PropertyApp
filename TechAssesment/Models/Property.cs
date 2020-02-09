6df8d3759bae01cc9a65e0f6e8e4990c23d69fa1
Victor Kurilo <victor_kurilo@hotmail.com>
2020-02-09 02:25:00 +0100

    Added Property model

---
+++ TechAssesment/Models/Property.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechAssesment.Models
+{
+    public class Property
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ApplicationUser Owner { get; set; }
+
+        public string OwnerId { get; set; }
+    }
+}
