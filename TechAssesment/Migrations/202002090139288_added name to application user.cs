8ea476b5596d7696d45d35b81ace9f15427aa19e
Victor Kurilo <victor_kurilo@hotmail.com>
2020-02-09 04:08:41 +0100

    updated migration with app user name

---
+++ TechAssesment/Migrations/202002090139288_added name to application user.cs
@@ -0,0 +1,18 @@
+﻿namespace TechAssesment.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addednametoapplicationuser : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Name", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Name");
+        }
+    }
+}
