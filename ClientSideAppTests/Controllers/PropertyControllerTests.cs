7ee9a7eff2db6f1ab059b1879ae3a21cf0d1f3ba
Victor Kurilo <victor_kurilo@hotmail.com>
2020-02-10 03:45:14 +0100

    Added unit test poject

---
+++ ClientSideAppTests/Controllers/PropertyControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ClientSideApp.Repositories;
+using ClientSideApp.ViewModels;
+using FluentAssertions;
+using Moq;
+
+namespace ClientSideApp.Controllers.Tests
+{
+    [TestClass]
+    public class PropertyControllerTests
+    {
+        private PropertyController _controller;
+        private Mock<IPropertyRepository> _mockService;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _mockService = new Mock<IPropertyRepository>();
+            _controller = new PropertyController(_mockService.Object);
+        }
+
+
+
+        [TestMethod]
+        public void Attend_UserAttendingAGigForWhichHeHasAnAttendance_ShouldReturnBadRequest()
+        {
+            var properties = new List<PropertyViewModel>()
+            {
+                new PropertyViewModel(){Id = 1, Name = "example1", Description = "example1"},
+                new PropertyViewModel(){Id = 2, Name = "example2", Description = "example2"},
+                new PropertyViewModel(){Id = 3, Name = "example3", Description = "example3"},
+                new PropertyViewModel(){Id = 4, Name = "example4", Description = "example4"},
+                new PropertyViewModel(){Id = 5, Name = "example5", Description = "example5"},
+                new PropertyViewModel(){Id = 6, Name = "example6", Description = "example6"},
+            };
+
+            _mockService.Setup(r => r.GetPropertyList()).Returns(properties);
+
+            var result = _controller.Index("name_desc", "", "", 5);
+
+            result.Should().Be(properties);
+        }
+    }
+}
---
+++ ClientSideAppTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ClientSideApp.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClientSideAppTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {
+            var controller = new PropertyController()
+            {
+
+            };
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange.
+            const int locationId = 1;
+            const LocationType locationType = LocationType.City;
+            int pageSize = 10;
+
+            // Act.
+            var postsPageOne = PostService.FindAllPostsForLocationPaged<Review>(locationId, locationType, 1, pageSize);
+            var postsPageTwo = PostService.FindAllPostsForLocationPaged<Review>(locationId, locationType, 2, pageSize);
+
+            // Assert.
+            Assert.IsTrue(postsPageOne.Count > 0);
+            Assert.IsTrue(postsPageTwo.Count > 0);
+            Assert.AreEqual(postsPageOne.Count, pageSize);
+            Assert.AreEqual(postsPageTwo.Count, pageSize);
+            CollectionAssert.AllItemsAreNotNull(postsPageOne.ToArray());
+            CollectionAssert.AllItemsAreNotNull(postsPageTwo.ToArray());
+        }
+    }
+}
