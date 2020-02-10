7c8576eeaf85a6bdff1055047f9f08435c563a3d
Victor Kurilo <victor_kurilo@hotmail.com>
2020-02-10 05:17:15 +0100

    ApplicationDbContext set to initialize by default

--- TechAssesment/Persistence/ApplicationDbContext.cs
+++ TechAssesment/Persistence/ApplicationDbContext.cs
@@ -13,8 +13,9 @@ namespace TechAssesment.Persistence
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+            Database.SetInitializer<ApplicationDbContext>(new CreateDatabaseIfNotExists<ApplicationDbContext>());
         }
-        
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
